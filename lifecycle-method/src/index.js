818d792b69a4866dd96e06576e143df06aae811f
tanj299 <tanj299@gmail.com>
2021-05-04 21:35:08 -0400

    Added Lifecycle method tests; added shopper React app

---
+++ lifecycle-method/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import LifecycleDemo from './Lifecycle'
+
+ReactDOM.render(<LifecycleDemo/>, 
+  document.getElementById('root')
+);
